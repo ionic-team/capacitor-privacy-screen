871bf33ddad064ec71ff56743eedbaeb27d735cd
jcesarmobile <jcesarmobile@gmail.com>
2026-07-23 18:28:40 +0200

    feat!: Align plugin baselines with core (#29)
    BREAKING CHANGE

--- Package.swift
+++ Package.swift
@@ -3,21 +3,20 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorPrivacyScreen",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "CapacitorPrivacyScreen",
             targets: ["PrivacyScreenPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5")
     ],
     targets: [
         .target(
             name: "PrivacyScreenPlugin",
             dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Capacitor", package: "capacitor-swift-pm")
             ],
             path: "ios/Sources/PrivacyScreenPlugin")
     ]
